140911ba8a6694e53d9efbce3727e9cdf173cc7c
dtsuper3 <dtsuper3@gmail.com>
2024-06-06 21:38:54 +0530

    number guess game created

---
+++ number-guess-game/src/main.rs
@@ -0,0 +1,54 @@
+use colored::*;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        Guess { value }
+    }
+
+    fn value(&self) -> u32 {
+        self.value
+    }
+}
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let guess = Guess::new(guess);
+        println!("Your guesses: {}", guess.value());
+
+        match guess.value().cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!".green());
+                break;
+            }
+        }
+    }
+}
